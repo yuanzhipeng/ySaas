4ea4a0b85e57f507d331c2da1b5677e0dc645b02
袁志鹏 <zhipengyuan3@creditease.cn>
2023-03-24 18:33:48 +0800

    修改nacos版本到1.4.1 降低nacosClient版本

--- y-platform/bff-web-2b/src/main/java/cc/sybx/saas/Web2bApplication.java
+++ y-platform/bff-web-2b/src/main/java/cc/sybx/saas/Web2bApplication.java
@@ -1,17 +1,42 @@
 package cc.sybx.saas;
 
 import cc.sybx.saas.common.configure.CompositePropertySourceFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@Slf4j
+@EnableJpaAuditing
+@EnableFeignClients
+@EnableDiscoveryClient
 @SpringBootApplication
-@PropertySource(value = {"web-base-application.properties", "application.properties", "api-application.properties"},
-        factory = CompositePropertySourceFactory.class)
+@PropertySource(value = {"web-base-application.properties","application.properties", "api-application.properties"}, factory = CompositePropertySourceFactory.class)
 public class Web2bApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Web2bApplication.class, args);
-    }
+    public static void main(String[] args) throws UnknownHostException {
+        System.setProperty("nacos.logging.default.config.enabled", "false");
+        System.setProperty("es.set.netty.runtime.available.processors", "false");
+        Environment env = SpringApplication.run(Web2bApplication.class, args).getEnvironment();
+        String port = env.getProperty("server.port", "8088");
+        String healthPort = env.getProperty("management.server.port", "9001");
 
+        log.info("Access URLs:\n----------------------------------------------------------\n\t"
+                        + "Local: \t\thttp://127.0.0.1:{}\n\t"
+                        + "External: \thttp://{}:{}\n\t"
+                        + "health: \thttp://{}:{}/act/health\n----------------------------------------------------------",
+                port,
+                InetAddress.getLocalHost().getHostAddress(),
+                port,
+                InetAddress.getLocalHost().getHostAddress(),
+                healthPort
+        );
+    }
 }
